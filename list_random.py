fafb8a172e86b3f4d2b23d6e4573f8bbc13b210d
Thomas Stassin <thomas.stassin@gsmproductions.com>
2022-09-30 11:26:56 +0200

    add a 6th element on list_random.py

--- list_random.py
+++ list_random.py
@@ -1,6 +1,6 @@
 from random import randint
 
 
-elements = ["terre", "air", "eau", "feu", "fer", "vide"]
-index = randint(0, 5)
+elements = ["terre", "air", "eau", "feu", "fer", "vide", "lumière"]
+index = randint(0, len(elements) - 1)
 print(elements[index])
